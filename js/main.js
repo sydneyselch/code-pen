6ad701f1f3797c70144ea7be9985a807b8e090e7
Sydney Selch <sydney_selch@Sydneys-MacBook-Pro.local>
2016-03-04 16:34:36 -0500

    feat(js): javascript added and connected

--- js/main.js
+++ js/main.js
@@ -1,74 +1,36 @@
-var window = ('body');
-
 // Sticky Header
 $(window).scroll(function() {
 
     if ($(window).scrollTop() > 100) {
-        $('.main_header').addClass('sticky');
+        $('.main_h').addClass('sticky');
     } else {
-        $('.main_header').removeClass('sticky');
+        $('.main_h').removeClass('sticky');
     }
 });
 
 // Mobile Navigation
 $('.mobile-toggle').click(function() {
-    if ($('.main_header').hasClass('open-nav')) {
-        $('.main_header').removeClass('open-nav');
+    if ($('.main_h').hasClass('open-nav')) {
+        $('.main_h').removeClass('open-nav');
     } else {
-        $('.main_header').addClass('open-nav');
+        $('.main_h').addClass('open-nav');
     }
 });
 
-$('.main_header li a').click(function() {
-    if ($('.main_header').hasClass('open-nav')) {
+$('.main_h li a').click(function() {
+    if ($('.main_h').hasClass('open-nav')) {
         $('.navigation').removeClass('open-nav');
-        $('.main_header').removeClass('open-nav');
+        $('.main_h').removeClass('open-nav');
     }
 });
 
-// navigation scroll
+// Navigation Scroll - ljepo radi materem
 $('nav a').click(function(event) {
     var id = $(this).attr("href");
-    var offset = 0;
+    var offset = 70;
     var target = $(id).offset().top - offset;
     $('html, body').animate({
         scrollTop: target
     }, 500);
     event.preventDefault();
-});
-/* Scroll-to-Top Button */
-$(window).scroll(function () {
-    if ($(this).scrollTop() > 100) {
-        $('.scrollup').fadeIn();
-    } else {
-        $('.scrollup').fadeOut();
-    }
-});
-
-$('.scrollup').click(function () {
-    $("html, body").animate({
-        scrollTop: 0
-    }, 600);
-    return false;
-});
-/* WORK IN PROGRESS
-   NAVIGATION ACTIVE STATE IN SECTION AREA
-*/
-var sections = $('section'), nav = $('nav'), nav_height = nav.outerHeight();
- 
-$(window).on('scroll', function () {
-  var cur_pos = $(this).scrollTop();
- 
-  sections.each(function() {
-    var top = $(this).offset().top - nav_height,
-        bottom = top + $(this).outerHeight();
- 
-    if (cur_pos >= top && cur_pos <= bottom) {
-      nav.find('a').removeClass('active');
-      sections.removeClass('active');
- 
-      $(this).addClass('active');
-      nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
-    }
-  });
 });
